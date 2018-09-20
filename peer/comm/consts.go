f7a9e1a5f8db5ff2467d91c54af0caaa12a1bdeb
mikelsr <mikelsr@pm.me>
2018-09-20 19:49:42 +0200

    Replace File messages with Block messages

--- peer/comm/consts.go
+++ peer/comm/consts.go
@@ -3,10 +3,10 @@ package comm
 const (
 	/* Message types (MT) */
 
-	// MTFileContent is a group of blocks forming a file
-	MTFileContent MessageType = iota
-	// MTFileRequest is used to ask for FileContent
-	MTFileRequest
+	// MTBlockContent is a group of blocks forming a file
+	MTBlockContent MessageType = iota
+	// MTBlockRequest is used to ask for BlockContent
+	MTBlockRequest
 	// MTIndexContent is the summary of a directory
 	MTIndexContent
 	// MTIndexRequest is used to ask for a IndexContent
