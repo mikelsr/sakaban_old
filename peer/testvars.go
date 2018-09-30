4ef1419fad41b763e2ea997b899406115597f046
mikelsr <mikelsr@pm.me>
2018-09-30 17:31:11 +0200

    Change testDir location

--- peer/testvars.go
+++ peer/testvars.go
@@ -2,6 +2,7 @@ package peer
 
 import (
 	"fmt"
+	"math/rand"
 
 	"bitbucket.org/mikelsr/sakaban-broker/broker"
 )
@@ -9,14 +10,14 @@ import (
 const (
 	testBrokerIP       = "127.0.0.1"
 	testBrokerPort     = 3080
-	testDir            = "/tmp/peertest"
 	testErrDialBackOff = "dial backoff"
-	testFailDir        = "/tmp/peertest/fail"
 )
 
 var (
 	testBrokerAddr = fmt.Sprintf("http://%s:%d", testBrokerIP, testBrokerPort)
 	testBroker     broker.Broker
+	testDir        = fmt.Sprintf("sakaban-test-%d", rand.Intn(1e8))
+	testFailDir    = testDir + "/fail"
 	testPeer       Peer
 	testIntPeer1   Peer // used for integration testing
 	testIntPeer2   Peer // used for integration testing
